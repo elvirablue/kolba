6c2a981f8a620d02ec9190fd20ea1b1cb5db42d0
elvirablue <olga_tretyakova@mail.ru>
2017-09-15 22:43:49 +0700

    +pravki

--- app/js/modal.js
+++ app/js/modal.js
@@ -3,29 +3,17 @@ $(document).on('ready', function(){
 //******************модальные окна для форм**************************************************
 
      
-   // $("#user_phone_modal").mask("+7 (999) 999-9999");
+  
    
     var popup_wrapper = document.querySelector('.modal_wrapper');
     var popup = document.querySelector('.js-modal');
-    var close = popup.querySelector('.modal_close');
-    var title = popup.querySelector('#modal_title');
-    var text = popup.querySelector('#modal_text');     
+    var close = popup.querySelector('.modal_close');         
 
     
-    $('.js-btn-modal-1').click(function(event){
+    $('.js-btn-modal').click(function(event){
           event.preventDefault();          
-          title.innerText  = 'Где находится код на упаковке?';
-          text.innerHTML = '<p>Код находится на задней стороне упаковки, в нижней части. Код покрыт защитным слоем, который нужно стереть. Если сохранность поля нарушена - обязательно проверьте код на сайте POLYMERIUM. Каждый код можно проверить всего 1 раз.</p>';
           PopupShow(event);
-    });
-    
-
-    $('.js-btn-modal-2').click(function(event){
-          event.preventDefault();          
-          title.innerText  = 'Почему ARMPACK- невозможно подделать?';
-          text.innerHTML = '<p>1. ARM PACK - на каждом продукте свой уникальный одноразовый код, тоесть если код был введен один раз - то больше воспользоваться им не возможно, соответственно "клонирование" кодов не возможно.</p><p>2. На этапах перевозки продукции и распространения, списать или скопировать код не возможно, он защищен специальным скретч покрытием, которое можно стереть только один раз.</p><p>3. Все оригинальные одноразовые коды с продуктов надежно хранятся в защищенной веб-базе POLYMERIUM и доступ к ним есть только у нескольких ответственных лиц компании.</p>';
-          PopupShow(event);
-    });
+    }); 
    
 
 
--- app/js/script.js
+++ app/js/script.js
@@ -29,7 +29,145 @@ $(document).on('ready', function(){
         event.preventDefault();
         var target = $(this);
         target.toggleClass('active');
-    });   	
+    }); 
+
+
+    $('#demo1').circliful({
+        // foreground Color
+          foregroundColor: "#ffdd00",
+
+          // backgroun color
+          backgroundColor: "#eee",
+
+          // fill color of point circle
+          pointColor: "none",
+
+          // fill color
+          fillColor: '#fff',
+
+          // width of foreground circle border
+          foregroundBorderWidth: 12,
+
+          // width of background circle border
+          backgroundBorderWidth: 12,
+
+          // font size of the percentage text
+          percentageTextSize: 42,
+
+
+          // font color
+          fontColor: '#000',
+
+          // from 0 to 100
+          percent: 76,
+
+          // additonal css for the percentage text
+            textAdditionalCss: 'graph__num',
+
+            animateInView: true,
+    });  	
+
+    $('#demo2').circliful({
+        // foreground Color
+          foregroundColor: "#ffdd00",
+
+          // backgroun color
+          backgroundColor: "#eee",
+
+          // fill color of point circle
+          pointColor: "none",
+
+          // fill color
+          fillColor: '#fff',
+
+          // width of foreground circle border
+          foregroundBorderWidth: 12,
+
+          // width of background circle border
+          backgroundBorderWidth: 12,
+
+          // font size of the percentage text
+          percentageTextSize: 42,
+
+
+          // font color
+          fontColor: '#000',
+
+          // from 0 to 100
+          percent: 92,
+
+          // additonal css for the percentage text
+            textAdditionalCss: 'graph__num',
+            animateInView: true,
+    }); 
+
+    $('#demo3').circliful({
+        // foreground Color
+          foregroundColor: "#ffdd00",
+
+          // backgroun color
+          backgroundColor: "#eee",
+
+          // fill color of point circle
+          pointColor: "none",
+
+          // fill color
+          fillColor: '#fff',
+
+          // width of foreground circle border
+          foregroundBorderWidth: 12,
+
+          // width of background circle border
+          backgroundBorderWidth: 12,
+
+          // font size of the percentage text
+          percentageTextSize: 42,
+
+
+          // font color
+          fontColor: '#000',
+
+          // from 0 to 100
+          percent: 23,
+
+          // additonal css for the percentage text
+        textAdditionalCss: 'graph__num',
+        animateInView: true,
+    });
+
+    $('#demo4').circliful({
+        // foreground Color
+          foregroundColor: "#ffdd00",
+
+          // backgroun color
+          backgroundColor: "#eee",
+
+          // fill color of point circle
+          pointColor: "none",
+
+          // fill color
+          fillColor: '#fff',
+
+          // width of foreground circle border
+          foregroundBorderWidth: 12,
+
+          // width of background circle border
+          backgroundBorderWidth: 12,
+
+          // font size of the percentage text
+          percentageTextSize: 42,
+
+
+          // font color
+          fontColor: '#000',
+
+          // from 0 to 100
+          percent: 47,
+
+          // additonal css for the percentage text
+            textAdditionalCss: 'graph__num',
+            animateInView: true,
+    });              
 
 
 
